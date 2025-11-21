cdf9cb091ecd23b80e020cbb2535b66dd66a011b
meltingseaweed <conor.seo@gmail.com>
2025-11-21 09:19:18 +0900

    clean up code make remove boxes function and remove console logs

--- java.js
+++ java.js
@@ -1,29 +1,25 @@
 //const { createElement } = require("react");
-
 const container = document.querySelector("#container");
 
 let numOfBoxes = 64;
+let opac = 0.1;
+createboxes(numOfBoxes);
+const boxes = document.querySelectorAll(".box")
+addColor(boxes);
+removeColor(boxes);
 
 function createboxes (num) {
     for (i = 0; i < numOfBoxes; i++) {
     const boxes= document.createElement("div");
     boxes.classList.toggle("box");
     container.appendChild(boxes);
+    }
 }
-}
-createboxes(numOfBoxes);
-
-const boxes = document.querySelectorAll(".box")
-addColor(boxes);
-removeColor(boxes);
-
 // Challenge 1: Randomize colors for mouse event
 // Challenge 2: Darkening Effect / Increase Opacity
-let opac = 0.1;
 function addColor (boxes) {
 boxes.forEach(box => {
     box.addEventListener("mouseenter", () => {
-        // Randomize color
         let r = Math.floor(Math.random() * 256 );
         let g = Math.floor(Math.random() * 256 );
         let b = Math.floor(Math.random() * 256 );
@@ -43,17 +39,19 @@ boxes.forEach(box => {
     });
 }); 
 }
+function removeBoxes () {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
     numOfBoxes = prompt("Enter a number between 1 and 100");
-    console.log(numOfBoxes);
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    opac = 0.1;
+    removeBoxes()
     createboxes(numOfBoxes);
     const boxes = document.querySelectorAll(".box")
+    opac = 0.1;
     addColor(boxes);
     removeColor(boxes);
 })
